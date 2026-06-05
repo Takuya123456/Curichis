5503e9156c2dfb8fffdd988c5675f681c73cb699
Takuya123456 <tacuyacanvara@gmail.com>
2026-06-04 21:15:29 -0500

    Delete app directory

--- app/controllers/LogoutController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class LogoutController extends Controller {
-    public function index() {
-        session_unset();
-        session_destroy();
-        $this->redirect('login');
-    }
-}
--- app/views/layouts/footer.php
+++
@@ -1,5 +0,0 @@
-    </main>
-</div>
-<script src="<?= APP_URL ?>/js/app.js"></script>
-</body>
-</html>
