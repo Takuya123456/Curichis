13a76c51b754353cddd6758fa7d855a51307bf79
Takuya123456 <tacuyacanvara@gmail.com>
2026-06-11 14:34:24 -0500

    Delete app directory

--- app/views/layouts/header-home.php
+++
@@ -1,5 +0,0 @@
-<div class="mobile-menu" id="mobileMenu">
-    <button class="close" id="closeMenu" aria-label="Cerrar menú">&times;</button>
-    <!-- Los links usan BASE_URL para que las rutas funcionen en cualquier entorno -->
-    <a href="<?php echo BASE_URL; ?>/login">Iniciar Sesión</a>
-</div>
